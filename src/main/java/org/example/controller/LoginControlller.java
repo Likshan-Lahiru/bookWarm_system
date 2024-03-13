d8fe8566eedf5071a590f9c05865b3eacecf668d
lahiru_lolitha <lahiru212001@gmail.com>
2024-03-13 20:21:01 +0530

    navigate signup button

--- src/main/java/org/example/controller/LoginControlller.java
+++ src/main/java/org/example/controller/LoginControlller.java
@@ -1,4 +1,41 @@
 package org.example.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginControlller {
+
+    @FXML
+    private JFXPasswordField txtLoginPagePassword;
+
+    @FXML
+    private JFXTextField txtLoginPageEmail;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) throws IOException {
+        AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/signUpForm.fxml"));
+        Scene scene = new Scene(anchorPane);
+        Stage stage =(Stage)root.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("SignUp Page");
+        stage.centerOnScreen();
+    }
+
 }
+
--- src/main/java/org/example/controller/SignUpController.java
+++ src/main/java/org/example/controller/SignUpController.java
@@ -1,4 +1,29 @@
 package org.example.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class SignUpController {
+
+    @FXML
+    private JFXTextField txtSignName;
+
+    @FXML
+    private JFXTextField txtSignEmail;
+
+    @FXML
+    private JFXPasswordField txtSignPassword;
+
+    @FXML
+    void btnLoginSignUpPage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignCreateAccount(ActionEvent event) {
+
+    }
+
 }
